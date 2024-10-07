a826e7d194cd2b99007ae8e940772b0de61ff873
Augustin BENGOLEA <abengolea.ext@softwaymedical.fr>
2024-10-07 17:29:25 +0200

    Add username CI variable to report infos

--- scripts/generate_report_specifics.sh
+++ scripts/generate_report_specifics.sh
@@ -23,6 +23,7 @@ CI_COMMIT_BRANCH=${CI_COMMIT_BRANCH:-$UNKNOWN}
 CI_COMMIT_SHORT_SHA=${CI_COMMIT_SHORT_SHA:-$UNKNOWN}
 CI_COMMIT_TITLE=${CI_COMMIT_TITLE:-$UNKNOWN}
 CI_PIPELINE_ID=${CI_PIPELINE_ID:-$UNKNOWN}
+GITLAB_USER_NAME=${GITLAB_USER_NAME:-$UNKNOWN}
 
 function update_report() {
   local tmp_file
@@ -31,14 +32,15 @@ function update_report() {
     \"branch\": \"$1\",
     \"commitSha\": \"$2\",
     \"commitTitle\": \"$3\",
-    \"pipelineId\": \"$4\"
+    \"pipelineId\": \"$4\",
+    \"username\": \"$5\",
   }]" "$ALL_REPORTS_INFO" > "$tmp_file"
   mv "$tmp_file" "$ALL_REPORTS_INFO"
 }
 
 echo "Before update"
 cat "$ALL_REPORTS_INFO"
-if ! update_report "$CI_COMMIT_BRANCH" "$CI_COMMIT_SHORT_SHA" "$CI_COMMIT_TITLE" "$CI_PIPELINE_ID"; then
+if ! update_report "$CI_COMMIT_BRANCH" "$CI_COMMIT_SHORT_SHA" "$CI_COMMIT_TITLE" "$CI_PIPELINE_ID" "$GITLAB_USER_NAME"; then
   echo "Error updating JSON" >&2
   exit 1
 fi
